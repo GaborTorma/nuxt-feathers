1e6c1090836a81422ae2614845fb2047763dc2ad
Gábor Torma <gabor@torma.dev>
2025-07-24 15:13:36 +0200

    fix(test): set nuxt environment for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
+    environment: 'nuxt',
     include: [
       './src/runtime/options/**/*.test.ts',
       './tests/vitest/*.test.ts',
